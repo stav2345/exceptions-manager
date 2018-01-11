6b7d4407698bd7516d5cecc25e093d13f55a62c7
ValentinoAvonEFSA <Valentino.AVON@ext.efsa.europa.eu>
2018-01-11 15:05:47 +0100

    first commit

---
+++ src/exception_manager/ExceptionConverter.java
@@ -0,0 +1,28 @@
+package exception_manager;
+
+public class ExceptionConverter {
+
+	/**
+	 * Convert the exception stack trace into a printable
+	 * string which contains all the information needed.
+	 * @param e
+	 * @return
+	 */
+	public static String getStackTrace(Exception e) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		String name = e.getClass().getName();
+		
+		sb.append(name).append("\n");
+		
+		String message = e.getMessage();
+		for (StackTraceElement ste : e.getStackTrace()) {
+	        sb.append("\n\tat ");
+	        sb.append(ste);
+	    }
+	    String trace = message + " " + sb.toString();
+	    
+	    return trace;
+	}
+}
